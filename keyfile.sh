39c3892f438c4800b9be8a43f5ae89cc51cd07a6
Ipetram <956771470@qq.com>
2016-07-17 16:30:17 +0800

    correct the wrong config

--- keyfile.sh
+++ keyfile.sh
@@ -6,3 +6,7 @@ sudo openssl rand -base64 741 > mongodb-keyfile
 ssh root@cfg3 'mkdir -p /home/core'
 sudo scp /home/core/mongodb-keyfile root@cfg3:/home/core/
 
+cd /home/core/
+sudo chmod 400 mongodb-keyfile
+sudo chown 999 mongodb-keyfile
+
